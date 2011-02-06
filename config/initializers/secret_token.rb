937c5e95a3ebbc7ddc97780ad3241f006166f858
rubyvim <rubyvim@gmail.com>
2011-02-06 14:04:57 -0500

    Done with demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'd660f613907cbe5349f99029ae41584595e2ef35aca7a81c1233fac13bb1471991e5b32407e2bec79e5505126e2508ad228649615a206361dd01b2c58f62d256'
